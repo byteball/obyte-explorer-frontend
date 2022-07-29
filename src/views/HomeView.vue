4c71cad21108d98e47cb97e89ebd7f1918a447cc
Evgenii Stulnikov <xxjenekxx@gmail.com>
2022-07-29 13:57:21 +0300

    - fixed overscroll

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,15 @@
 <script setup>
 import UnitComponent from "../components/unit/Index.vue";
 import UnitInfo from "../components/unit/UnitInfo.vue";
+import { onMounted, onUnmounted } from "vue";
+
+onMounted(() => {
+  document.body.classList.add("overscroll-none");
+});
+
+onUnmounted(() => {
+  document.body.classList.remove("overscroll-none");
+});
 </script>
 
 <template>
