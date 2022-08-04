c491ded8d0b6e614b0c9998593ea37a205cdcaeb
Evgenii Stulnikov <xxjenekxx@gmail.com>
2022-08-04 20:45:51 +0300

    - type

--- src/plugins/socket.io.js
+++ src/plugins/socket.io.js
@@ -5,7 +5,7 @@ import { useAssetNamesStore } from "../stores/assetNames";
 export const socketIoPlugin = {
   install: (app, options) => {
     if (!options.url) {
-      throw new Error("Please set VITE_WS_URL");
+      throw new Error("Please set VITE_API_URL");
     }
     const ratesStore = useRatesStore();
     const assetNamesStore = useAssetNamesStore();
