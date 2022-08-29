b3dce22ec65e030d825c8169ce9ffb1886126dc9
Evgenii Stulnikov <xxjenekxx@gmail.com>
2022-08-29 21:16:15 +0300

    search button ghost

--- src/components/elements/Navbar.vue
+++ src/components/elements/Navbar.vue
@@ -144,7 +144,8 @@ function searchBlur() {
             class="input input-ghost w-full max-w-xs text-base pr-2.5 hover:outline-0 focus:outline-0"
           />
           <button
-            class="btn btn-square bg-blue-500 border-blue-500 hover:bg-blue-600 hover:border-blue-600 text-white btn-sm w-10 h-10"
+            class="btn btn-square btn-ghost text-blue-500 btn-sm w-10 h-10"
+            style="border: 1px solid rgba(33, 33, 33, 0.2); border-left: none"
             @click="searchEventHandler"
           >
             <svg
