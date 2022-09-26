daa71f89300d1bc97dd7876928dff2edcbc7fd2a
Evgenii Stulnikov <xxjenekxx@gmail.com>
2022-09-26 19:51:10 +0300

    prepareLink for units starting with a slash

--- src/components/elements/Link.vue
+++ src/components/elements/Link.vue
@@ -14,10 +14,18 @@ if (props.type === "address") {
 if (props.type === "asset") {
   path.value += "asset/";
 }
+
+function prepareLink(link) {
+  if (link.startsWith("/")) {
+    return link.replace("/", "%2F");
+  }
+
+  return link;
+}
 </script>
 
 <template>
-  <router-link class="link link-hover text-blue-500" :to="path + link">
+  <router-link class="link link-hover text-blue-500" :to="path + prepareLink(link)">
     <slot></slot>
   </router-link>
 </template>
