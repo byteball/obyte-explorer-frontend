3458ca55cabe6f974d8a8ec9835d07612cde3901
xMarioInc <lchaosmachinel@gmail.com>
2024-05-13 20:16:50 +0300

    added urls from doc for AA to address page

--- components/address/AADescription.vue
+++ components/address/AADescription.vue
@@ -2,12 +2,22 @@
 const props = defineProps(["definition", "baseAaDefinition"]);
 
 const aaDescription = ref('');
+const aaHomepageUrl = ref('');
+const aaSourceUrl = ref('');
 
 const fetchAndSetAaDescription = async (docUrl) => {
   const aaDoc = await $fetch(docUrl);
 
   if (aaDoc.description) {
     aaDescription.value = aaDoc.description;
+  }
+
+  if (aaDoc.homepage_url) {
+    aaHomepageUrl.value = aaDoc.homepage_url;
+  }
+
+  if (aaDoc.source_url) {
+    aaSourceUrl.value = aaDoc.source_url;
   }
 }
 
@@ -37,5 +47,13 @@ await checkAndSetAaDescription();
 <template>
   <div v-if="aaDescription" class="mt-1">
     {{ aaDescription }}
+  </div>
+  <div class="mt-1">
+    <div v-if="aaHomepageUrl" class="inline-block">
+      <NuxtLink class="link link-hover text-blue-500" :to="aaHomepageUrl" target="_blank"> Home page </NuxtLink>
+    </div>
+    <div v-if="aaSourceUrl" class="ml-2 inline-block">
+      <NuxtLink class="link link-hover text-blue-500" :to="aaSourceUrl" target="_blank"> Source </NuxtLink>
+    </div>
   </div>
 </template>
