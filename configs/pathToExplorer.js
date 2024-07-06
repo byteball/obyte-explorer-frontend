9e37b078cdf72e8b1634f21683886023d2535812
Evgenii Stulnikov <xxjenekxx@gmail.com>
2024-07-07 00:12:09 +0300

    fixed path for devnet

--- configs/pathToExplorer.js
+++ configs/pathToExplorer.js
@@ -2,7 +2,7 @@ import { isDevNet } from "./isDevNet.js";
 
 let path = '';
 if (isDevNet) {
-  path = 'http://localhost:4000';
+  path = 'http://localhost:8080';
 } else {
   path = import.meta.env.VITE_TESTNET ? 'https://testnetexplorer.obyte.org' : 'https://explorer.obyte.org';
 }
